1d32008823db895039fc3852a52a84c13f52b7df
Bayram Barış Gök <bayrambarisgok>
2024-07-11 14:23:32 +0300

    Clock
    Her dakika başında uyarı verir

---
+++ Clock/clock.js
@@ -0,0 +1,34 @@
+let zaman_al = document.querySelector(".saat");
+let uyari_al = document.querySelector(".uyari");
+
+let saat = () =>{
+
+    let zaman = new Date();
+    let saat_al = zaman.getHours();
+    let dakika_al = zaman.getMinutes();
+    let saniye_al = zaman.getSeconds();
+    console.log(saat_al,dakika_al,saniye_al);
+
+    let html_göm = `
+    
+        <span> ${saat_al} : </span>
+        <span> ${dakika_al} : </span>
+        <span> ${saniye_al} </span>
+
+    `
+    zaman_al.innerHTML = html_göm;
+
+    if(saniye_al === 0){
+
+        uyari_al.innerHTML = "Dakika Doldu";
+    }
+    else{
+
+        uyari_al.innerHTML= "";
+    }
+
+    
+}
+
+
+setInterval(saat,1000);
